0fc8a08941f504e1546639a9cc6aa6efe0ec9669
Ankkkkkkit <ankitsingh02003@gmail.com>
2024-07-07 21:42:57 +0530

    Initialized spring boot project

---
+++ src/main/java/com/content/aggregator/scrapper/ContentAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.content.aggregator.scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/content/aggregator/scrapper/ContentAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.content.aggregator.scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
